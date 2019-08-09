dd52c56e5a6199c5edb9b21d34d9cf6c37e98e65
xxxeonm <xeonmin0215@naver.com>
2019-08-09 21:39:20 +0900

    UPDATE: access detail board through link pre-processing

--- interparktour/run.py
+++ interparktour/run.py
@@ -39,11 +39,24 @@ driver.implicitly_wait(10)
 driver.find_element_by_css_selector('.oTravelBox>.boxList>.moreBtnWrap>.moreBtn').click()
 print("CLICK 'MORE'")
 
+### implicit waits
+driver.implicitly_wait(10)
+
 ### access 3rd tour detail board
-third_tour_info = driver.find_elements_by_css_selector('.oTravelBox>.boxList>li')[2]
-third_tour_info.find_element_by_css_selector('img').click()
+third_tour = driver.find_elements_by_css_selector('.oTravelBox>.boxList>li')[2]
+print(third_tour.find_element_by_css_selector('a').get_attribute('onclick'))
+tmp_strs = third_tour.find_element_by_css_selector('a').get_attribute('onclick').split(',')
+if tmp_strs:
+    third_link = tmp_strs[0].replace('searchModule.OnClickDetail(', '')[1:-1]
+print(third_link)
 # driver.find_element_by_css_selector('.oTravelBox>.boxList>li')[2].find_element_by_css_selector('img').click()
 print("CLICK THIRD DETAIL BOARD IMAGE")
 
+### implicit waits
+driver.implicitly_wait(10)
 
+### print third detail board info
+#third_tour_info = driver.find_element_by_class_name('info-list')
+
+#print(third_tour_info.text)
 
