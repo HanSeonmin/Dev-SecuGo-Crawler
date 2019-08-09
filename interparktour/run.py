e8a23eaeab55495bb492dfdac3f6b507e9ebba18
xxxeonm <xeonmin0215@naver.com>
2019-08-09 17:16:37 +0900

    UPDATE: access third detail board

--- interparktour/run.py
+++ interparktour/run.py
@@ -29,10 +29,21 @@ driver.find_element_by_id("SearchGNBText").send_keys(keyword)
 
 ### click search button
 driver.find_element_by_css_selector('.search-btn').click() 
+print("CLICK SEARCH BUTTON")
 #driver.find_element_by_css_selector('button.search_btn').click()
 
 ### implicit waits
 driver.implicitly_wait(10)
 
-### click "더보기"
+### click "더보기" -> access tour list
 driver.find_element_by_css_selector('.oTravelBox>.boxList>.moreBtnWrap>.moreBtn').click()
+print("CLICK 'MORE'")
+
+### access 3rd tour detail board
+third_tour_info = driver.find_elements_by_css_selector('.oTravelBox>.boxList>li')[2]
+third_tour_info.find_element_by_css_selector('img').click()
+# driver.find_element_by_css_selector('.oTravelBox>.boxList>li')[2].find_element_by_css_selector('img').click()
+print("CLICK THIRD DETAIL BOARD IMAGE")
+
+
+
